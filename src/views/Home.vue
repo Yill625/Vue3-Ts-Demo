9374982fe7c89f93c3402b98b7e8efb79f8b1533
Ivan <1095036273@qq.com>
2021-01-25 16:48:37 +0800

    按需引入vant

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,19 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Wel11222333333come to Your Vue.js + TypeScript App" />
   </div>
+  <van-button type="primary" block>块级元素</van-button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Button } from "vant";
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    [Button.name]: Button
   }
 });
 </script>
