b55de1470f1abe25f7268a24d5f0cb9245c1b811
Ivan <1095036273@qq.com>
2021-02-03 19:24:00 +0800

    使用betterscroll

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,15 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about.[hash]" */ '../views/About.vue')
+  },
+  {
+    path: '/scroll',
+    name: 'Scroll',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "scroll.[hash]" */ '../views/Scroll.vue')
   }
 ]
 
---
+++ src/views/Scroll.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="wrapper">
+    <div class="content">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <HelloWorld msg="Wel11222333333come to Your Vue.js + TypeScript App" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { Button } from 'vant'
+import BScroll from '@better-scroll/core'
+
+export default defineComponent({
+  name: 'Scroll',
+  components: {
+    HelloWorld,
+    [Button.name]: Button
+  },
+  mounted() {
+    setTimeout(() => {
+      new BScroll('.wrapper')
+    }, 20)
+  }
+})
+</script>
+
+<style lang="css" scoped>
+.wrapper {
+  height: 600px;
+  overflow: hidden;
+}
+.content {
+}
+</style>
